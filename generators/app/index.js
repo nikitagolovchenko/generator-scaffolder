1b60bea07ed0dbe079748800f55bf742f2cee050
Aleksey Rozhkov <aleksey.rozhkov@p2h.com>
2019-04-15 12:05:56 +0300

    Reverted version after tests

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,6 +9,16 @@ const PROMPTS = require('./prompts');
 const WRITING = require('./writing');
 const VALUES = require('./globals');
 const { PROMPTS_VALUES } = require('./globals');
+const notifier = updateNotifier({pkg, updateCheckInterval: 1000 * 60 * 60 * 24});
+
+let yosayPrompts = (props) => {
+  if (props) {
+    return `Update available:
+       ${chalk.red(props.current)} → ${chalk.green(props.latest)}.
+       Run ${chalk.blue(`npm i -g generator-p2h`)} to update`
+  }
+  return `2005-${new Date().getFullYear()} All rights Reserved. P2H, Inc.`
+}
 
 function frameworkCopy(self) {
   self.log(
@@ -65,12 +75,19 @@ module.exports = class extends Generator {
 
     // Next, add your custom code
     this.option('babel'); // This method adds support for a `--babel` flag
-    updateNotifier({pkg}).notify();
   }
 
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay('2005-2018 All rights Reserved. P2H, Inc.'));
+
+    if (notifier.update) {
+      this.log(yosay(yosayPrompts({
+        current: notifier.update.current,
+        latest: notifier.update.latest,
+      })));
+    } else {
+      this.log(yosay(yosayPrompts()));
+    }
 
     return this.prompt(PROMPTS).then(props => {
       // To access props later use this.props.someAnswer;
