96800d61b1b044587bc689e94dccf10d7d4f0df3
Aleksey Rozhkov <aleksey.rozhkov@p2h.com>
2020-02-21 11:40:29 +0200

    small changes

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,18 +1,16 @@
 const fs = require('fs');
-const Generator = require('yeoman-generator');
-
-// const updateNotifier = require('update-notifier');
 const chalk = require('chalk');
 const yosay = require('yosay');
-
-// const pkg = require('../../package.json');
+const Generator = require('yeoman-generator');
+const updateNotifier = require('update-notifier');
 const PROMPTS = require('./prompts');
 const WRITING = require('./writing');
+const pkg = require('../../package.json');
 const {PROMPTS_VALUES} = require('./globals');
 const addWPHeader = require('./extends/wordpressHeader');
-// const notifier = updateNotifier({pkg, updateCheckInterval: 1000 * 60 * 60 * 24});
+const notifier = updateNotifier({pkg, updateCheckInterval: 1000 * 60 * 60 * 24});
 
-let yosayPrompts = props => {
+const yosayPrompts = props => {
   if (props) {
     return `Update available:
        ${chalk.red(props.current)} → ${chalk.green(props.latest)}.
@@ -21,31 +19,21 @@ let yosayPrompts = props => {
   return `2005-${new Date().getFullYear()} All rights Reserved. P2H, Inc.`;
 };
 
-module.exports = class extends Generator {
+class WebpackGenerator extends Generator {
   constructor(args, opts) {
-    // Calling the super constructor is important so our generator is correctly set up
     super(args, opts);
-
-    // Next, add your custom code
-    this.option('babel'); // This method adds support for a `--babel` flag
+    this.option('babel');
   }
 
   async prompting() {
-    // Have Yeoman greet the user.
-
-    // if (notifier.update) {
-    //   this.log(
-    //     yosay(
-    //       yosayPrompts({
-    //         current: notifier.update.current,
-    //         latest: notifier.update.latest,
-    //       })
-    //     )
-    //   );
-    // } else {
-    // }
-    this.log(yosay(yosayPrompts()));
-
+    if (notifier.update) {
+      this.log(yosay(yosayPrompts({
+        current: notifier.update.current,
+        latest: notifier.update.latest,
+      })));
+    } else {
+      this.log(yosay(yosayPrompts()));
+    }
 
     this.props = await this.prompt(PROMPTS);
   }
@@ -70,3 +58,5 @@ module.exports = class extends Generator {
     this.log(chalk.green(`🙌 Installation done! 🙌 For ${chalk.yellow('development mode')} run command ${chalk.red('npm run dev')} OR ${chalk.red('yarn dev')} from markup folder 👊. For more info, read ${chalk.yellow('README.md')}`));
   }
 };
+
+module.exports = WebpackGenerator;
