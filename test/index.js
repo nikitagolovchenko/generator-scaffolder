3604ba211e7fe76186bf07cc40737bd7a38e1e4d
Aleksey Rozhkov <aleksey.rozhkov@p2h.com>
2020-03-09 18:45:28 +0200

    Added test for markup/tailwind/no-linters

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
-describe('Markup Only tests', () => {
+describe('Markup', () => {
   require('./markupOnly/m_noFrames_noLint');
   require('./markupOnly/m_TB_noLint');
   require('./markupOnly/m_Zurb_noLint');
   require('./markupOnly/m_materialize_noLint');
+  require('./markupOnly/m_tailwind_noLint');
 
   after(function () {
     console.log("after all tests");
