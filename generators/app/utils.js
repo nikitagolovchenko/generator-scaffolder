81d59c111d0c59a849b7b17b053074915787a4d0
Aleksey Rozhkov <aleksey.rozhkov@p2h.com>
2020-04-11 13:07:51 +0300

    reverted utility

--- generators/app/utils.js
+++ generators/app/utils.js
@@ -40,4 +40,33 @@ const projectTypeMessage = prompts => {
   return chalk.blue(message)
 }
 
-module.exports = {getFilesArray, setProcessToDestination, projectTypeMessage};
+/**
+ * Performs a deep merge of `source` into `target`.
+ * Mutates `target` only but not its objects and arrays.
+ *
+ * @author inspired by [jhildenbiddle](https://stackoverflow.com/a/48218209).
+ */
+function mergeDeep(target, source) {
+  const isObject = (obj) => obj && typeof obj === 'object';
+
+  if (!isObject(target) || !isObject(source)) {
+    return source;
+  }
+
+  Object.keys(source).forEach(key => {
+    const targetValue = target[key];
+    const sourceValue = source[key];
+
+    if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+      target[key] = targetValue.concat(sourceValue);
+    } else if (isObject(targetValue) && isObject(sourceValue)) {
+      target[key] = mergeDeep(Object.assign({}, targetValue), sourceValue);
+    } else {
+      target[key] = sourceValue;
+    }
+  });
+
+  return target;
+}
+
+module.exports = {getFilesArray, setProcessToDestination, projectTypeMessage, mergeDeep};
